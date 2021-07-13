76db70af7d7ba6875c3dc578f521b8df5e632aac
Sébastien DEJEAN <sebastien.dejean@gmail.com>
2021-07-13 09:41:34 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var array = [{ shape: 'square', color: 'red', used: 1, instances: 1 }, { shape: 'square', color: 'red', used: 2, instances: 1 }, { shape: 'circle', color: 'blue', used: 0, instances: 0 }, { shape: 'square', color: 'blue', used: 4, instances: 4 }, { shape: 'circle', color: 'red', used: 1, instances: 1 }, { shape: 'circle', color: 'red', used: 1, instances: 0 }, { shape: 'square', color: 'blue', used: 4, instances: 5 }, { shape: 'square', color: 'red', used: 2, instances: 1 }],
- 
-hash = Object.create(null),
-grouped = [];
-    
-var keys_grouping = ['shape', 'color'];
-var keys_summing = ['instances'];
-
-var final_keys = [...keys_grouping, ...keys_summing];
+   
 	
 function initObject(o) {
   var rv = {};
@@ -23,7 +15,14 @@ function initObject(o) {
   return rv;
 }
 	
-array.forEach(function (o) {
+exports.groupby = function(array, keys_grouping, keys_summing) {
+  console.log("This is a message from the demo package");
+
+	var final_keys = [...keys_grouping, ...keys_summing];
+	var hash = Object.create(null),
+	grouped = [];
+
+  array.forEach(function (o) {
     var key = keys_grouping.map(function (k) { return o[k]; }).join('|');
     if (!hash[key]) {
 		hash[key] = initObject(o);	
@@ -31,9 +30,5 @@ array.forEach(function (o) {
     }
     keys_summing.forEach(function (k) { hash[key][k] += o[k]; });
 });
-
-console.log('final_keys', final_keys);
-
-console.log('source', array);
-console.log('grouped', grouped);
-
+  
+}
